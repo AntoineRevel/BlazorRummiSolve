b7a86deac402b2e74163eeda6dd04b3aebe9b52c
Antoine Revel <revel.antoine@gmail.com>
2024-08-24 11:01:58 +0200

    Uncomment BenchmarkRunner to run performance tests
    Removed commented-out code to enable executing the benchmark tests. This allows us to gather performance metrics for the RummiBench class, contributing to performance optimization efforts.

--- RummiSolve/Program.cs
+++ RummiSolve/Program.cs
@@ -6,7 +6,7 @@ public static class Program
 {
     public static void Main()
     {
-        //BenchmarkRunner.Run<RummiBench>();
+        BenchmarkRunner.Run<RummiBench>();
         // PlaySoloGame();
     }
 
