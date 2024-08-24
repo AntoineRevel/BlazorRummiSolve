9fb72b7aa426dfe72a57d17b8cbedd3b0d1b23db
Antoine Revel <revel.antoine@gmail.com>
2024-08-24 23:59:31 +0200

    .env to gitignore

--- RummiSolve/Program.cs
+++ RummiSolve/Program.cs
@@ -1,13 +1,40 @@
 ﻿using BenchmarkDotNet.Running;
+using StackExchange.Redis;
 
 namespace RummiSolve;
 
 public static class Program
 {
-    public static void Main()
+    private static void Main(string[] args)
     {
-        BenchmarkRunner.Run<RummiBench>();
-        // PlaySoloGame();
+        // Configuration de la connexion
+        var options = new ConfigurationOptions
+        {
+            EndPoints = { "192.168.1.88:6379" },
+            User = "Rummi.net", // Nom d'utilisateur
+            Password = "votre_mot_de_passe", // Mot de passe de l'utilisateur
+            AbortOnConnectFail = false, // Permet de ne pas échouer immédiatement en cas de problème de connexion
+            Ssl = false // Activer si vous utilisez SSL
+        };
+
+        try
+        {
+            // Création de la connexion
+            using (var connection = ConnectionMultiplexer.Connect(options))
+            {
+                // Accéder à la base de données Redis
+                IDatabase db = connection.GetDatabase();
+
+                // Exemple d'opération : SET et GET d'une clé
+                db.StringSet("cle", "valeur");
+                string valeur = db.StringGet("cle");
+                Console.WriteLine($"La valeur pour 'cle' est : {valeur}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erreur lors de la connexion à Redis : {ex.Message}");
+        }
     }
 
     public static void TestBench()
