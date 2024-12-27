38e16149c10ff2d1353d2ed20949b1ab1faff95b
Antoine Revel <revel.antoine@gmail.com>
2024-12-27 10:07:08 +0100

    big facto

--- RummiSolve/RummiSolve/Solver/BinaryFirstSolver.cs
+++ RummiSolve/RummiSolve/Solver/BinaryFirstSolver.cs
@@ -1,18 +1,11 @@
 namespace RummiSolve.Solver;
 
-public class BinaryFirstSolver
+public class BinaryFirstSolver : SolverBase
 {
-    private readonly Tile[] _tiles;
-    private readonly bool[] _usedTiles;
-    private int _jokers;
-    public Solution BestSolution { get; private set; } = new();
     public required IEnumerable<Tile> TilesToPlay { get; init; }
 
-    private BinaryFirstSolver(Tile[] tiles, int jokers)
+    private BinaryFirstSolver(Tile[] tiles, int jokers) : base(tiles, jokers)
     {
-        _tiles = tiles;
-        _jokers = jokers;
-        _usedTiles = new bool[tiles.Length];
     }
 
     public static BinaryFirstSolver Create(List<Tile> playerTiles)
@@ -46,7 +39,7 @@ public class BinaryFirstSolver
 
     public bool SearchSolution()
     {
-        BestSolution = FindSolution(new Solution(), 0,0);
+        BestSolution = FindSolution(new Solution(), 0, 0);
 
         return BestSolution.IsValid;
     }
@@ -54,7 +47,7 @@ public class BinaryFirstSolver
 
     private Solution FindSolution(Solution solution, int solutionScore, int startIndex)
     {
-        startIndex = Array.FindIndex(_usedTiles, startIndex, used => !used);
+        startIndex = Array.FindIndex(UsedTiles, startIndex, used => !used);
 
         if (startIndex == -1) return solution;
 
@@ -74,7 +67,7 @@ public class BinaryFirstSolver
         Action<Solution, TS> addSetToSolution)
         where TS : ValidSet
     {
-        _usedTiles[firstUnusedTileIndex] = true;
+        UsedTiles[firstUnusedTileIndex] = true;
         foreach (var set in sets)
         {
             MarkTilesAsUsed(set, true, firstUnusedTileIndex);
@@ -94,157 +87,8 @@ public class BinaryFirstSolver
             MarkTilesAsUsed(set, false, firstUnusedTileIndex);
         }
 
-        _usedTiles[firstUnusedTileIndex] = false;
+        UsedTiles[firstUnusedTileIndex] = false;
 
         return solution;
-    }
-
-    private void MarkTilesAsUsed(ValidSet set, bool isUsed, int firstUnusedIndex)
-    {
-        foreach (var tile in set.Tiles[0].IsJoker ? set.Tiles.Skip(2) : set.Tiles.Skip(1))
-        {
-            for (var i = firstUnusedIndex + 1; i < _tiles.Length; i++)
-            {
-                if (_usedTiles[i] == isUsed || !_tiles[i].Equals(tile)) continue;
-
-                _usedTiles[i] = isUsed;
-                break;
-            }
-        }
-
-        _jokers += isUsed ? -set.Jokers : set.Jokers;
-    }
-
-    private IEnumerable<Run> GetRuns(int tileIndex)
-    {
-        var availableJokers = _jokers;
-        var color = _tiles[tileIndex].Color;
-        var currentRun = new List<Tile> { _tiles[tileIndex] };
-        var i = tileIndex + 1;
-
-        while (true)
-        {
-            for (; i < _tiles.Length; i++)
-            {
-                if (_tiles[i].Color != color)
-                {
-                    i = _tiles.Length;
-                    break;
-                }
-
-                if (_usedTiles[i] || _tiles[i].Value == currentRun[^1].Value) continue;
-
-                if (_tiles[i].Value != currentRun[^1].Value + 1) break;
-
-                currentRun.Add(_tiles[i]);
-
-                if (currentRun.Count < 3) continue;
-
-                var jokersUsed = _jokers - availableJokers;
-
-                yield return new Run
-                {
-                    Tiles = [..currentRun],
-                    Jokers = jokersUsed
-                };
-            }
-
-            if (availableJokers <= 0) yield break;
-
-            if (currentRun[^1].Value != 13) currentRun.Add(new Tile(currentRun[^1].Value + 1, color, true));
-
-            else if (currentRun[0].Value != 1) currentRun.Insert(0, new Tile(currentRun[0].Value - 1, color, true));
-
-            availableJokers--;
-
-            if (currentRun.Count < 3) continue;
-
-            var jokersUsedJ = _jokers - availableJokers;
-
-            yield return new Run
-            {
-                Tiles = [..currentRun],
-                Jokers = jokersUsedJ
-            };
-        }
-    }
-
-    private IEnumerable<Group> GetGroups(int firstTileIndex)
-    {
-        var sameNumberTiles = _tiles
-            .Where((tile, index) => !_usedTiles[index] &&
-                                    tile.Value == _tiles[firstTileIndex].Value &&
-                                    tile.Color != _tiles[firstTileIndex].Color)
-            .Distinct()
-            .ToList();
-
-        var size = sameNumberTiles.Count;
-
-        if (_jokers == 0)
-        {
-            if (size < 2) yield break;
-
-            var groupTiles = new List<Tile> { _tiles[firstTileIndex] };
-
-            groupTiles.AddRange(sameNumberTiles);
-
-            yield return new Group { Tiles = groupTiles.ToArray() };
-
-            if (groupTiles.Count != 4) yield break;
-
-            for (var i = 0; i < sameNumberTiles.Count; i++)
-            for (var j = i + 1; j < sameNumberTiles.Count; j++)
-            {
-                yield return new Group
-                {
-                    Tiles = [_tiles[firstTileIndex], sameNumberTiles[i], sameNumberTiles[j]]
-                };
-            }
-        }
-        else
-        {
-            for (var jokersUsed = 0; jokersUsed <= _jokers; jokersUsed++)
-            for (var tilesUsed = 2; tilesUsed <= size + 1; tilesUsed++)
-            {
-                var totalTiles = tilesUsed + jokersUsed;
-                if (totalTiles is < 3 or > 4) continue;
-
-                var combinations = GetCombinations(sameNumberTiles, tilesUsed - 1);
-
-                foreach (var tiles in combinations)
-                {
-                    var groupTiles = new Tile[totalTiles];
-
-                    groupTiles[0] = _tiles[firstTileIndex];
-
-                    for (var i = 0; i < tilesUsed - 1; i++) groupTiles[i + 1] = tiles[i];
-
-                    for (var k = 0; k < jokersUsed; k++)
-                        groupTiles[tilesUsed + k] = new Tile(_tiles[firstTileIndex].Value, isJoker: true);
-
-                    yield return new Group
-                    {
-                        Tiles = groupTiles,
-                        Jokers = jokersUsed
-                    };
-                }
-            }
-        }
-    }
-
-
-    private static IEnumerable<List<Tile>> GetCombinations(List<Tile> list, int length)
-    {
-        if (length == 0) yield return [];
-
-        for (var i = 0; i < list.Count; i++)
-        {
-            var element = list[i];
-            foreach (var combination in GetCombinations(list.Skip(i + 1).ToList(), length - 1))
-            {
-                combination.Add(element);
-                yield return combination;
-            }
-        }
     }
 }
--- RummiSolve/RummiSolve/Solver/BinarySolver.cs
+++ RummiSolve/RummiSolve/Solver/BinarySolver.cs
@@ -36,11 +36,11 @@ public class BinarySolver : SolverBase
         };
     }
 
-    protected override bool ValidateCondition()
+    private bool ValidateCondition()
     {
         if (UsedTiles.Any(b => !b)) return false;
 
-        return Jokers == 0 ;
+        return Jokers == 0;
     }
 
 
@@ -57,7 +57,7 @@ public class BinarySolver : SolverBase
         startIndex = Array.FindIndex(UsedTiles, startIndex, used => !used);
 
         if (startIndex == -1) return solution;
-        
+
         var solRun = TrySet(GetRuns(startIndex), solution, startIndex,
             (sol, run) => sol.AddRun(run));
 
@@ -96,154 +96,5 @@ public class BinarySolver : SolverBase
         UsedTiles[firstUnusedTileIndex] = false;
 
         return solution;
-    }
-
-    private void MarkTilesAsUsed(ValidSet set, bool isUsed, int firstUnusedIndex)
-    {
-        foreach (var tile in set.Tiles[0].IsJoker ? set.Tiles.Skip(2) : set.Tiles.Skip(1))
-        {
-            for (var i = firstUnusedIndex + 1; i < Tiles.Length; i++)
-            {
-                if (UsedTiles[i] == isUsed || !Tiles[i].Equals(tile)) continue;
-
-                UsedTiles[i] = isUsed;
-                break;
-            }
-        }
-
-        Jokers += isUsed ? -set.Jokers : set.Jokers;
-    }
-
-    private IEnumerable<Run> GetRuns(int tileIndex)
-    {
-        var availableJokers = Jokers;
-        var color = Tiles[tileIndex].Color;
-        var currentRun = new List<Tile> { Tiles[tileIndex] };
-        var i = tileIndex + 1;
-
-        while (true)
-        {
-            for (; i < Tiles.Length; i++)
-            {
-                if (Tiles[i].Color != color)
-                {
-                    i = Tiles.Length;
-                    break;
-                }
-
-                if (UsedTiles[i] || Tiles[i].Value == currentRun[^1].Value) continue;
-
-                if (Tiles[i].Value != currentRun[^1].Value + 1) break;
-
-                currentRun.Add(Tiles[i]);
-
-                if (currentRun.Count < 3) continue;
-
-                var jokersUsed = Jokers - availableJokers;
-
-                yield return new Run
-                {
-                    Tiles = [..currentRun],
-                    Jokers = jokersUsed
-                };
-            }
-
-            if (availableJokers <= 0) yield break;
-
-            if (currentRun[^1].Value != 13) currentRun.Add(new Tile(currentRun[^1].Value + 1, color, true));
-
-            else if (currentRun[0].Value != 1) currentRun.Insert(0, new Tile(currentRun[0].Value - 1, color, true));
-
-            availableJokers--;
-
-            if (currentRun.Count < 3) continue;
-
-            var jokersUsedJ = Jokers - availableJokers;
-
-            yield return new Run
-            {
-                Tiles = [..currentRun],
-                Jokers = jokersUsedJ
-            };
-        }
-    }
-
-    private IEnumerable<Group> GetGroups(int firstTileIndex)
-    {
-        var sameNumberTiles = Tiles
-            .Where((tile, index) => !UsedTiles[index] &&
-                                    tile.Value == Tiles[firstTileIndex].Value &&
-                                    tile.Color != Tiles[firstTileIndex].Color)
-            .Distinct()
-            .ToList();
-
-        var size = sameNumberTiles.Count;
-
-        if (Jokers == 0)
-        {
-            if (size < 2) yield break;
-
-            var groupTiles = new List<Tile> { Tiles[firstTileIndex] };
-
-            groupTiles.AddRange(sameNumberTiles);
-
-            yield return new Group { Tiles = groupTiles.ToArray() };
-
-            if (groupTiles.Count != 4) yield break;
-
-            for (var i = 0; i < sameNumberTiles.Count; i++)
-            for (var j = i + 1; j < sameNumberTiles.Count; j++)
-            {
-                yield return new Group
-                {
-                    Tiles = [Tiles[firstTileIndex], sameNumberTiles[i], sameNumberTiles[j]]
-                };
-            }
-        }
-        else
-        {
-            for (var jokersUsed = 0; jokersUsed <= Jokers; jokersUsed++)
-            for (var tilesUsed = 2; tilesUsed <= size + 1; tilesUsed++)
-            {
-                var totalTiles = tilesUsed + jokersUsed;
-                if (totalTiles is < 3 or > 4) continue;
-
-                var combinations = GetCombinations(sameNumberTiles, tilesUsed - 1);
-
-                foreach (var tiles in combinations)
-                {
-                    var groupTiles = new Tile[totalTiles];
-
-                    groupTiles[0] = Tiles[firstTileIndex];
-
-                    for (var i = 0; i < tilesUsed - 1; i++) groupTiles[i + 1] = tiles[i];
-
-                    for (var k = 0; k < jokersUsed; k++)
-                        groupTiles[tilesUsed + k] = new Tile(Tiles[firstTileIndex].Value, isJoker: true);
-
-                    yield return new Group
-                    {
-                        Tiles = groupTiles,
-                        Jokers = jokersUsed
-                    };
-                }
-            }
-        }
-    }
-
-
-    private static IEnumerable<List<Tile>> GetCombinations(List<Tile> list, int length)
-    {
-        if (length == 0) yield return [];
-
-        for (var i = 0; i < list.Count; i++)
-        {
-            var element = list[i];
-            foreach (var combination in GetCombinations(list.Skip(i + 1).ToList(), length - 1))
-            {
-                combination.Add(element);
-                yield return combination;
-            }
-        }
     }
 }
