8370593a6e42484fb2a1615a9b4468b58be026c4
Antoine Revel <revel.antoine@gmail.com>
2025-01-09 17:01:03 +0100

    ok async first

--- RummiSolve/RummiSolve/Solver/Combinations/First/CombinationsFirstSolver.cs
+++ RummiSolve/RummiSolve/Solver/Combinations/First/CombinationsFirstSolver.cs
@@ -42,36 +42,39 @@ public class CombinationsFirstSolver(List<Tile> tiles) : ISolver
 
         tiles.Reverse();
 
-        BinaryFirstBaseSolver? bestSolver = null;
+        //BinaryFirstBaseSolver? bestSolver = null;
 
         for (var tileTry = tiles.Count - 1; tileTry > 2; tileTry--)
         {
-            //     Parallel.ForEach(BaseSolver.GetCombinations(tiles, tileTry),
-            //         (combi, loopState) =>
+            // var size = tileTry;
+            // Parallel.ForEach(BaseSolver.GetCombinations(tiles, tileTry),
+            //     (combi, loopState) =>
+            //     {
+            //         if (bestSolver != null)
             //         {
-            //             if (bestSolver != null)
-            //             {
-            //                 loopState.Stop();
-            //                 return;
-            //             }
-            //             
-            //             var joker = combi.Count(tile => tile.IsJoker);
-            //             var solver = new BinaryFirstBaseSolver(combi.ToArray(), joker);
-            //             var found = solver.SearchSolution();
-            //             if (!found) return;
-            //             bestSolver = solver;
             //             loopState.Stop();
-            //         });
-            //     
-            //     if (bestSolver == null) continue;
-            //     
-            //     Found = true;
-            //     BestSolution = bestSolver.BinarySolution;
-            //     TilesToPlay = bestSolver.TilesToPlay;
-            //     JokerToPlay = bestSolver.JokerToPlay;
-            //     return;
+            //             return;
+            //         }
+            //         
+            //         var joker = combi.Count(tile => tile.IsJoker);
+            //         if (joker > 0) combi.RemoveRange(size - joker, joker);
+            //         var solver = new BinaryFirstBaseSolver(combi.ToArray(), joker);
+            //         var found = solver.SearchSolution();
+            //         if (!found) return;
+            //         bestSolver = solver;
+            //         loopState.Stop();
+            //     });
+            //
+            // if (bestSolver == null) continue;
+            //
+            // Found = true;
+            // BestSolution = bestSolver.BinarySolution;
+            // TilesToPlay = bestSolver.TilesToPlay;
+            // JokerToPlay = bestSolver.JokerToPlay;
+            // return;
 
-            foreach (var combi in BaseSolver.GetCombinations(tiles, tileTry))
+            foreach (var combi in 
+                     BaseSolver.GetCombinations(tiles, tileTry).OrderByDescending(l => l.Sum(t => t.Value)))
             {
                 var joker = combi.Count(tile => tile.IsJoker);
                 if (joker > 0) combi.RemoveRange(tileTry - joker, joker);
