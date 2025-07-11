09027f6ceabb73b122273c83f1aa9ac883e40002
Antoine Revel <revel.antoine@gmail.com>
2025-07-11 15:37:10 +0200

    Refactor solver strategies to use `Set` instead of `Solution` for board representation.

--- RummiSolve/RummiSolve/Strategy/ISolverStrategy.cs
+++ RummiSolve/RummiSolve/Strategy/ISolverStrategy.cs
@@ -4,6 +4,6 @@ namespace RummiSolve.Strategy;
 
 public interface ISolverStrategy
 {
-    Task<SolverResult> GetSolverResult(Solution boardSolution, Set rack, bool hasPlayed,
-        CancellationToken cancellationToken);
+    Task<SolverResult> GetSolverResult(Set board, Set rack, bool hasPlayed,
+        CancellationToken cancellationToken = default);
 }
